66a8592101abb51056c85802bb9821c2097a07fd
Norio Kobota <nori.0428@gmail.com>
2011-07-05 19:51:55 +0900

    my local config commited, sorry.

--- src/samples/netstat/src/netstat_impl.sh
+++ src/samples/netstat/src/netstat_impl.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while [ 1 ]; do
-	(sleep 2; LANG=en; netstat -atn | sed -e 's/[^0-9a-zA-Z. :\*]//g') | telnet ::1 9100 >/dev/null
+	(sleep 2; LANG=en; netstat -atn | sed -e 's/[^0-9a-zA-Z. :\*]//g') | telnet 127.0.0.1 9000 >/dev/null
 	sleep 1
 done
 
