138bd208d9af70ea0657be295d334790e93271e9
Norio Kobota <nori.0428@gmail.com>
2013-11-22 16:00:35 +0900

    remove old samples

--- src/samples/netstat/src/netstat.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-impl="sh ./netstat_impl.sh"
-nohup $impl >&- 2>&- <&- & 
-[ $? -ne 0 ] && echo NG || echo OK
--- src/samples/netstat/src/netstat_impl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-while [ 1 ]; do
-	(sleep 2; LANG=en; netstat -atn | sed -e 's/[^0-9a-zA-Z. :\*]//g') | telnet 127.0.0.1 9000 >/dev/null
-	sleep 1
-done
-
-# EOF
