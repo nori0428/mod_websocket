1f9868c593b58713d254c4bd407625da483a77bd
Norio Kobota <nori.0428@gmail.com>
2014-10-03 16:21:53 +0900

    resolve issue #40 by using poll. And revise algorithm to connect.(for IPv6)

--- src/mod_websocket_socket.c
+++ src/mod_websocket_socket.c
@@ -12,6 +12,7 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <errno.h>
+#include <poll.h>
 
 #include "mod_websocket_socket.h"
 
@@ -19,93 +20,51 @@ int mod_websocket_connect(const char *host, const char *service) {
     struct addrinfo hints;
     struct addrinfo *res = NULL;
     struct addrinfo *ai = NULL;
-    struct fdlist {
-        int fd;
-        struct fdlist *next;
-    };
-    struct fdlist *head = NULL, *p, *pn, *fde = NULL;
-    int flags, fd, maxfd = -1, connfd = -1, sockret = -1;
+    int flags, fd = -1, sockret = -1;
     socklen_t socklen = sizeof(sockret);
-    fd_set fds;
-    struct timeval tv;
+    struct pollfd pollfd;
 
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = 0;
-
     if (getaddrinfo(host, service, &hints, &res) != 0) {
         return -1;
     }
-    FD_ZERO(&fds);
     for (ai = res; ai; ai = ai->ai_next) {
         fd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
         if (fd < 0) {
-            goto go_out;
+          break;
         }
         if ((flags = fcntl(fd, F_GETFL, 0)) < 0 ||
             fcntl(fd, F_SETFL, flags | O_NONBLOCK) < 0) {
             close(fd);
-            goto go_out;
+            fd = -1;
+            continue;
         }
-        fde = (struct fdlist *)malloc(sizeof(struct fdlist));
-        if (!fde) {
+        pollfd.fd = fd;
+        pollfd.events = POLLOUT;
+        if (connect(fd, ai->ai_addr, ai->ai_addrlen) < 0 &&
+            errno != EINPROGRESS) {
             close(fd);
-            goto go_out;
+            fd = -1;
+            continue;
         }
-        fde->fd = fd;
-        fde->next = head;
-        head = fde;
-        /**
-         * for https://github.com/nori0428/mod_websocket/issues/40
-         * 'FD_SET' occurs buffer overflow when fd > 1024
-         *
-         * mod_websocket set only one fd into 'fd_set' in most cases,
-         * but fd number takes over 1024 when many websocket connection exist.
-         *
-         * http://stackoverflow.com/questions/7976388/increasing-limit-of-fd-setsize-and-select
-         */
-        if ((fde->fd % FD_SETSIZE) > maxfd) {
-            maxfd = fde->fd % FD_SETSIZE;
+        if (poll(&pollfd, 1, 5000) == 0) {
+            close(fd);
+            fd = -1;
+            continue;
         }
-        FD_SET((fde->fd % FD_SETSIZE), &fds);
-        if (connect(fde->fd, ai->ai_addr, ai->ai_addrlen) < 0) {
-            if (errno != EINPROGRESS) {
-                goto go_out;
-            }
+        if ((pollfd.revents & POLLOUT) &&
+            getsockopt(fd, SOL_SOCKET, SO_ERROR, &sockret, &socklen) == 0 &&
+            sockret == 0 ) {
+            break;
         }
-    }
-    /* connect timeout is to set 5 secs */
-    tv.tv_sec = 5;
-    tv.tv_usec = 0;
-    if (select(maxfd + 1, NULL, &fds, NULL, &tv) == 0) {
-        goto go_out;
-    } else {
-        for (p = head; p; p = p->next) {
-            if (!FD_ISSET((p->fd % FD_SETSIZE), &fds)) {
-                continue;
-            }
-            if (getsockopt(p->fd, SOL_SOCKET, SO_ERROR,
-                           &sockret, &socklen) == 0 &&
-                sockret == 0) {
-                connfd = p->fd;
-                break;
-            }
-        }
-    }
-
- go_out:
-    p = head;
-    while (p) {
-        if (p->fd != connfd) {
-            close(p->fd);
-        }
-        pn = p->next;
-        free(p);
-        p = pn;
+        close(fd);
+        fd = -1;
     }
     freeaddrinfo(res);
-    return connfd;
+    return fd;
 }
 
 void mod_websocket_disconnect(int fd) {
